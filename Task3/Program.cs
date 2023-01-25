48be0d20fd780a5e51baec6ec5fddb62ad4a62a3
SergeyMikheev <s.a.mikheev@yandex.ru>
2023-01-25 03:59:42 +0300

    apdate Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -6,3 +6,15 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+Console.WriteLine("Введите номер дня");
+int day = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(day % 7);
+if (day % 7 > 0 & day % 7 <= 5)
+{
+  Console.WriteLine("Будний день");
+}
+else
+{
+  Console.WriteLine("Выходной день");
+}
