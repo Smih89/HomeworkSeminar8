03f1d894b61445e730b2cfdcf39747ab67c4680d
SergeyMikheev <s.a.mikheev@yandex.ru>
2023-01-25 03:53:04 +0300

    apdate Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -6,3 +6,16 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+
+Console.Write("Введите целое, трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (100 <= number && number <= 999)
+{
+  number /= 10;
+  number = number % 10;
+  Console.WriteLine(number);
+}
+else
+{
+Console.WriteLine("Число не соответствует условию ввода");
+}
